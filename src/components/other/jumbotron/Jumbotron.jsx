9871a9c5785bb777988b02bb02693639daf96334
ChahturaMathula <chathuraekanayake927@hotmail.com>
2023-12-14 15:59:16 +0530

    implement jumbotron

--- src/components/other/jumbotron/Jumbotron.jsx
+++ src/components/other/jumbotron/Jumbotron.jsx
@@ -1,11 +1,19 @@
 import "../../../styles/Jumbotron.css";
+import SecondaryColorButton from "../../ui/buttons/SecondaryColorButton";
 
 export default function Jumbotron() {
     return (
         <div className="jumbotron__container">
-            <img className="jumbotron__image" alt="jumbotron image" />
+            <picture className="jumbotron__image" >
+                <source media="(max-width:576px)" srcset="./assets/images/main-image-xs.png" />
+                <source media="(max-width:768px)" srcset="./assets/images/main-image-s.png" />
+                <source media="(max-width:992px)" srcset="./assets/images/main-image-l.png" />
+                <source media="(min-width:1200px)" srcset="./assets/images/main-image-xl.png" />
+                <img alt="jumbotron image" src="./assets/images/main-image-xl.png" />
+            </picture>
             <div className="jumbotron-heading__container">
                 <p>We crush your competitors, goals, and sales records - without the B.S.</p>
+                <SecondaryColorButton>GET FREE CONSULTATION</SecondaryColorButton>
             </div>
 
         </div>
