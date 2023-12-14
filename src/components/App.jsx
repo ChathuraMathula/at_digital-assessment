be1706a1b13eb06f4dd00a552ff4909565169401
ChahturaMathula <chathuraekanayake927@hotmail.com>
2023-12-14 14:52:24 +0530

    implement jumbotron

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import '../styles/App.css'
 import Header from './header/Header'
+import Jumbotron from './other/jumbotron/Jumbotron'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Jumbotron />
     </>
   )
 }
---
+++ src/components/other/jumbotron/Jumbotron.jsx
@@ -0,0 +1,13 @@
+import "../../../styles/Jumbotron.css";
+
+export default function Jumbotron() {
+    return (
+        <div className="jumbotron__container">
+            <img className="jumbotron__image" alt="jumbotron image" />
+            <div className="jumbotron-heading__container">
+                <p>We crush your competitors, goals, and sales records - without the B.S.</p>
+            </div>
+
+        </div>
+    );
+}
