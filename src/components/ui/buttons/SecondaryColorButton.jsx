132aaeaf778f5f7bbd302ac981cd71a893b5acf0
ChahturaMathula <chathuraekanayake927@hotmail.com>
2023-12-14 17:56:46 +0530

    refactored

--- src/components/ui/buttons/SecondaryColorButton.jsx
+++
@@ -1,9 +0,0 @@
-import "../../../styles/SecondaryColorButton.css";
-
-export default function SecondaryColorButton({ children, onClick }) {
-    return (
-        <button className="secondary-color__button" onClick={onClick}>
-            {children}
-        </button>
-    );
-}
