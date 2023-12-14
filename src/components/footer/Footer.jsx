6b28de451498d3520811aa55cac2eb12c8f2a0f2
ChahturaMathula <chathuraekanayake927@hotmail.com>
2023-12-14 19:50:56 +0530

    implement footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,27 @@
 import "../../styles/Footer.css";
 import FooterDescription from "./FooterDescription";
+import FooterLink from "./FooterLink";
+import FooterLinkContainer from "./FooterLinkContainer";
 
 export default function Footer() {
     return (
         <footer>
             <FooterDescription />
+            <FooterLinkContainer title="Our Technologies">
+                <FooterLink>ReactJS</FooterLink>
+                <FooterLink>Gatsby</FooterLink>
+                <FooterLink>NextJS</FooterLink>
+                <FooterLink>NodeJS</FooterLink>
+                <FooterLink>GraphQL</FooterLink>
+                <FooterLink>Laravel</FooterLink>
+            </FooterLinkContainer>
+            <FooterLinkContainer title="Our Services">
+                <FooterLink>Social media Marketing</FooterLink>
+                <FooterLink>Web & Mobile App Development</FooterLink>
+                <FooterLink>Data & Analytics</FooterLink>
+                <FooterLink>Google Marketing solutions</FooterLink>
+                <FooterLink>Search Engine Optimization</FooterLink>
+            </FooterLinkContainer>
         </footer>
     );
 }
--- src/components/footer/FooterLinkContainer.jsx
+++ src/components/footer/FooterLinkContainer.jsx
@@ -2,12 +2,12 @@
 
 import "../../styles/FooterLinkContainer.css";
 
-export default function FooterLinkContainer({ title }) {
+export default function FooterLinkContainer({ title, children }) {
     return (
         <div className="footer-link__container">
             <h3>{title}</h3>
             <ul>
-
+                {children}
             </ul>
         </div>
     );
